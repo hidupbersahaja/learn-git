bb4f54c39284a0109f7b80e2b81951e3c79f8828
Abdullah 'Alim (alimshare) <alimm.abdullah@gmail.com>
2019-01-30 06:06:10 +0700

    add wrong syntax which make error in application

--- kacang-ijo/src/feature1.java
+++ kacang-ijo/src/feature1.java
@@ -2,5 +2,6 @@
 public class feature1 {
 	public static void main(String[]args) {
 		System.out.println("feature 1 ");
+		System.err.println("Something went wrong... application shutting down !")
 	}
 }
